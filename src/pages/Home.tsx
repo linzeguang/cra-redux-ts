90d1f3c66036a7b4ab92f356b51c1cfdab2a75df
linzeguang <15817289730@163.com>
2023-02-13 10:26:36 +0800

    feat: init mui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { selectTheme, updateTheme } from '@/features/basicSlice'
@@ -10,8 +11,12 @@ const Home: React.FC = () => {
   return (
     <div>
       <div>{theme}</div>
-      <button onClick={() => dispatch(updateTheme('dark'))}>dark</button>
-      <button onClick={() => dispatch(updateTheme('light'))}>light</button>
+      <Button variant='contained' onClick={() => dispatch(updateTheme('dark'))}>
+        dark
+      </Button>
+      <Button variant='outlined' onClick={() => dispatch(updateTheme('light'))}>
+        light
+      </Button>
     </div>
   )
 }
